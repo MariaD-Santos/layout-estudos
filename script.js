ce9957edcf04e570177be7e43f6ff64af1f348d2
MariaD-Santos <maria.j.santos108@aluno.senai.br>
2026-04-10 08:58:30 -0300

    JS e refatoração

--- script.js
+++ script.js
@@ -1,48 +1,79 @@
-
-
-
-const colorPicker = document.querySelector('#colorPicker');
+const colorPicker = document.querySelector('#color-picker');
 const fontSelect = document.querySelector('#font-select');
 const themeToggle = document.querySelector('#theme-toggle');
 const articleColorPicker = document.querySelector('#article-color-picker');
 const body = document.querySelector('body');
-const newsArticles= document.querySelector('.news');
+const newsArticles = document.querySelector('.news');
 
-/* Carrega as preferencias do usuário do localStorage e aplica as configurações na página
-@function loadPreferences
-*/
-const loadPreferences = () => {
+
+const loadPreferences = () =>{
     const savedColor = localStorage.getItem('primaryColor');
-    if (savedColor) {
-        document.docuementElement.style.setProperty('--primary-color', savedColor);
+    if (savedColor){
+        document.documentElement.setProperty('--primary-color', savedColor);
         colorPicker.value = savedColor;
     }
+
+    const savedArticleColor = localStorage.getItem('articleColor');
+    if (savedArticleColor) {
+        document.documentElement.setProperty('--primary-color', savedColor);
+        colorPicker.value = savedColor;
+    }
+
+
+    const savedFont = localStorage.getItem('fontFamily');
+    if (savedFont) {
+        document.documentElement.setProperty('--font-family', savedFont);
+        localStorage.setItem = savedFont;
+    }
+
+
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        body.classList.add('dark');
+        themeToggle.innerHTML = '<span class="material-icons">light_mode</span>'
+    }
+
+
 };
 
-    colorPicker.addEventListener('input', (e) => {
-        const color = e.target.value; // captura o valor selecionado no color picker
-        document.documentElement.style.setProperty('--primary-color', color);
-        localStorage.setItem('primaryColor', color);
-    });
 
-    articleColorPicker.addEventListener('input', (e) => {
-        const color = e.target.value;
-        newsArticles.forEach(article =>{
-            article.style.backgroundColor = color;
-        })
-        localStorage.setItem('articleColor', color);
-    });
+colorPicker.addEventListener('input', (e) => {
+    const color = e.target.value;
+
+    document.documentElement.style.setProperty('--primaryColor', color)
+
+    localStorage.setItem('primaryColor', color)
+
+});
+
+articleColorPicker.addEventListener('input', (e) => {
+    const color = e.target.value;
+    newsArticles.forEach(article => {article.style.backgroundColor = color})
+
+    localStorage.addEventListener('articleColor', color)
+
+});
 
 fontSelect.addEventListener('change', (e) => {
-
+    const font = e.target.value;
+    document.documentElement.style.setProperty('--font-family', font)
+    localStorage.setItem('fontFamily', font)
 });
+
 
 themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    const isDark = body.classList.contains('dark');
 
+    themeToggle.innerHTML = isDark
+    ? '<span class="material-icons">light_mode</span>'
+    : '<span class="material-icons">dark_mode</span>';
+
+    localStorage.setItem('theme', isDark ? 'dark' : 'light')
 });
 
 
-
+loadPreferences();
 
 
 
